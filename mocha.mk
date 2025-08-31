74616b51adaf3790dd5596be1fff3763f8322e6a
Dargons10 <jdamr2p@gmail.com>
2025-08-31 22:36:56 +0200

    mocha: device: Set api level in 33

--- mocha.mk
+++ mocha.mk
@@ -19,6 +19,9 @@ LOCAL_PATH := device/xiaomi/mocha
 $(call inherit-product-if-exists, vendor/xiaomi/mocha/mocha-vendor.mk)
 $(call inherit-product-if-exists, vendor/xiaomi/mocha/consolemode-blobs.mk)
 
+# API
+PRODUCT_PACKAGES += $(PRODUCT_PACKAGES_SHIPPING_API_LEVEL_33)
+
 #APEX
 PRODUCT_COMPRESSED_APEX := false
 
