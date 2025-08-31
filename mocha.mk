f23546bd90ba1a6c7b929be939a007cac8f93f60
Dargons10 <jdamr2p@gmail.com>
2025-08-31 22:37:59 +0200

    mocha: device: Enable hwbinder BT

--- mocha.mk
+++ mocha.mk
@@ -64,8 +64,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libbt-vendor \
     android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service \
-    android.hardware.bluetooth.audio@2.0-impl
+    android.hardware.bluetooth@1.0-service
 
 # Camera
 #PRODUCT_COPY_FILES += \
